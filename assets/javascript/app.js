c68e581f2655acde70e9a92858415923ad58af01
chuman <42072991+chrishuman0923@users.noreply.github.com>
2018-09-27 21:26:29 -0400

    Adds config data for Firebase DB

---
+++ assets/javascript/app.js
@@ -0,0 +1,13 @@
+// Initialize Firebase
+var config = {
+    apiKey: "AIzaSyAcgdpSSSobcgp2hFf1wSu9yNf8Roe6WIQ",
+    authDomain: "moviedb-db817.firebaseapp.com",
+    databaseURL: "https://moviedb-db817.firebaseio.com",
+    projectId: "moviedb-db817",
+    storageBucket: "moviedb-db817.appspot.com",
+    messagingSenderId: "817296084015"
+  };
+
+  firebase.initializeApp(config);
+  
+//BEGIN JS CODE FOR APP BELOW THIS POINT
