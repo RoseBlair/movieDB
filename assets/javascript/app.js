9a62cdcbf1a4dbb985e71b6904df4dd87fcdc71d
chuman <42072991+chrishuman0923@users.noreply.github.com>
2018-09-29 14:11:31 -0400

    Adds variable objects showing base url's for TMDB search and poster retrieval

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -6,8 +6,17 @@ var config = {
     projectId: "moviedb-db817",
     storageBucket: "moviedb-db817.appspot.com",
     messagingSenderId: "817296084015"
-  };
+  },
+  db = firebase.database();
 
   firebase.initializeApp(config);
   
 //BEGIN JS CODE FOR APP BELOW THIS POINT
+var tmdbSearch = {
+  baseUrl: "https://api.themoviedb.org/3/search/movie?",
+  apiKey: "951021584287e01bb1ba8473989df6da"
+},
+tmdbImg = {
+  baseUrl: "https://image.tmdb.org/t/p/w500/",
+  size: "w500",
+};
