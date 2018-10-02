cee20203bb516c5f904e060795747d8d09aafbb7
chuman <42072991+chrishuman0923@users.noreply.github.com>
2018-10-01 20:04:08 -0400

    Searches for and adds movie posters for searched movies

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -6,17 +6,60 @@ var config = {
     projectId: "moviedb-db817",
     storageBucket: "moviedb-db817.appspot.com",
     messagingSenderId: "817296084015"
-  },
-  db = firebase.database();
+  };
 
   firebase.initializeApp(config);
   
 //BEGIN JS CODE FOR APP BELOW THIS POINT
-var tmdbSearch = {
-  baseUrl: "https://api.themoviedb.org/3/search/movie?",
-  apiKey: "951021584287e01bb1ba8473989df6da"
-},
-tmdbImg = {
-  baseUrl: "https://image.tmdb.org/t/p/w500/",
-  size: "w500",
-};
+var $movieSearch = $("#movieSearch"),
+    tmdb = {
+      searchUrl: "https://api.themoviedb.org/3/search/movie?",
+      imageUrl: "https://image.tmdb.org/t/p/w500",
+      apiKey: "api_key=951021584287e01bb1ba8473989df6da",
+      lang: "&language=en-US"
+    },
+    db = firebase.database();
+
+function searchForMovies(title) {
+  //create request url
+  var url = [tmdb.searchUrl, tmdb.apiKey, tmdb.lang, "&query=", title].join("");
+
+  $.ajax({
+    url: url,
+    method: "GET"
+
+  }).then(function (resp) {
+    //get results and pass them to next function
+    displaySearchResults(resp.results);
+
+  }).catch(function (error) {
+    //catch and log error from api request
+    console.log(`Error: ${error}`);
+  });
+}
+
+function displaySearchResults(resp) {
+  var resultLimit;
+
+  if (resp.length > 10) {
+    resultLimit = 10;
+  } else {
+    resultLimit = resp.length;
+  }
+
+  for (var i = 0; i < resultLimit; i++) {
+    var currentResp = resp[i],
+        src = [tmdb.imageUrl, currentResp.poster_path].join("");
+    
+    if (currentResp.poster_path) {
+      var newMovie = $("<img>");
+        
+      newMovie.attr({
+          "src": src,
+          "alt": currentResp.title
+      }).addClass("searchedMovie");
+
+      $movieSearch.append(newMovie);
+    }
+  }
+}
