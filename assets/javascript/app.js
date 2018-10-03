7be2d8b4e633843ab760db311ef5e88f288ccdba
chuman <42072991+chrishuman0923@users.noreply.github.com>
2018-10-03 17:07:10 -0400

    Adds existing collection to DOM

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -23,58 +23,58 @@ var $movieSearch = $("#movieSearch"),
     db = firebase.database(),
     movieIDs;
 
-function movieSearch(title) {
+//Adds movie data to database
+function addMovieToDb(movieID, movieTitle) {
+  db.ref().push({
+    id: parseInt(movieID),
+    title: movieTitle.trim().toLowerCase()
+  });
+}
+
+function searchMovie(title) {
   //create request url
   var url = [tmdb.searchUrl, tmdb.apiKey, tmdb.lang, "&query=", title].join("");
 
   $.ajax({
     url: url,
     method: "GET"
-
   }).then(function (resp) {
-    //get results and pass them to next function
     displayMovieSearch(resp.results);
-
   }).catch(function (error) {
-    //catch and log error from api request
     console.log(`Error: ${error}`);
   });
 }
 
 function displayCollection() {
+  //clear DOM
+  $movieCollection.empty();
+
   for (var i = 0; i < movieIDs.length; i++) {
     var url = [tmdb.idUrl, movieIDs[i], "?", tmdb.apiKey, tmdb.lang].join("");
 
     $.ajax({
       url: url,
       method: "GET"
-  
-    }).then(function (resp) {
-      //loops through results and appends movie poster to DOM
-      for (var x = 0; x < resp; x++) {
-        var currentResp = resp[x],
-            src = [tmdb.imageUrl, currentResp.poster_path].join("");
+    }).then(function (resp) {        
+      //if movie poster exists...
+      if (resp.poster_path) {
+        var src = [tmdb.imageUrl, resp.poster_path].join(""),
+            newDiv = $("<div>"),
+            movie = $("<img>");
         
-        //if movie poster exists add it to DOM
-        if (currentResp.poster_path) {
-          var newDiv = $("<div>"),
-              movie = $("<img>");
-          
-          //adds attributes to movie poster image
-          movie.attr({
-              "src": src,
-              "alt": currentResp.title,
-          });
+        //adds attributes to movie poster image
+        movie.attr({
+            "src": src,
+            "alt": resp.title,
+        }).addClass("img-fluid");
 
-          //appends movie poster and btn to div
-          newDiv.append(movie).addClass("inCollection");
+        //appends poster to div
+        newDiv.append(movie).addClass("inCollection");
 
-          //appends created div to DOM
-          $movieCollection.append(newDiv);
-        }
+        //appends created div to DOM
+        $movieCollection.append(newDiv);
       }
     }).catch(function (error) {
-      //catch and log error from api request
       console.log(`Error: ${error}`);
     });
   }
@@ -83,7 +83,7 @@ function displayCollection() {
 function displayMovieSearch(resp) {
   var resultLimit;
 
-  //limits results to 10 or max of returned if less than 10 results
+  //limits results to 10 or max of returned, if less than 10 results
   if (resp.length > 10) {
     resultLimit = 10;
   } else {
@@ -92,32 +92,35 @@ function displayMovieSearch(resp) {
 
   //loops through results and appends movie poster to DOM
   for (var i = 0; i < resultLimit; i++) {
-    var currentResp = resp[i],
-        src = [tmdb.imageUrl, currentResp.poster_path].join("");
+    var currResp = resp[i],
+        src = [tmdb.imageUrl, currResp.poster_path].join("");
     
-    //if movie poster exists add it to DOM
-    if (currentResp.poster_path) {
+    //if movie poster exists, add it to DOM
+    if (currResp.poster_path) {
       var newDiv = $("<div>"),
-          newMovie = $("<img>"),
-          addBtn = $("<btn>");
+          movie = $("<img>"),
+          btn = $("<btn>");
       
       //adds attributes to movie poster image
-      newMovie.attr({
+      movie.attr({
           "src": src,
-          "alt": currentResp.title,
+          "alt": currResp.title,
       });
 
-      //determines which button to display depending on if the movie is already in the database
-      if (movieIDs.indexOf(currentResp.id) === -1) {
+      //change display of button if the movie is already in the database
+      if (movieIDs.indexOf(currResp.id) === -1) {
         //movie not in collection
-        addBtn.text("Add to Library").addClass("btn btn-primary addLib").attr("data-id", currentResp.id);
+        btn.attr({
+            "data-id": currResp.id,
+            "data-title": currResp.title
+          }).text("Add to Library").addClass("btn btn-primary addLib");
       } else {
-        //movie in collection already
-        addBtn.html("&#9747; Already in Library").addClass("btn btn-secondary disabled");
+        //movie already in collection
+        btn.html("&#9747; Already in Library").addClass("btn btn-secondary disabled");
       }
 
       //appends movie poster and btn to div
-      newDiv.append(newMovie, addBtn).addClass("searchedMovie");
+      newDiv.append(movie, btn).addClass("searchedMovie");
 
       //appends created div to DOM
       $movieSearch.append(newDiv);
@@ -125,24 +128,19 @@ function displayMovieSearch(resp) {
   }
 }
 
-function addMovieToDb(movieID) {
-  db.ref().push({
-    id: parseInt(movieID)
-  });
-}
-
-//event listener to get movie id from "add to library" button and pass into another function
+//event listener for "Add to Library" btn
 $(document).on("click", ".addLib", function() {
   var btn = $(this),
-      id = btn.attr("data-id");
+      id = btn.attr("data-id"),
+      title = btn.attr("data-title");
 
-    addMovieToDb(id);
+    addMovieToDb(id, title);
 
     //disable button and change text
     btn.html("&#10004; Added to Library").addClass("disabled btn-secondary").removeClass("btn-primary");
 });
 
-//when movies are added or removed from the database, update the local array
+//when movies are added or removed from the database or when the page first loads, update the local array
 db.ref().on("value", function(snapshot) {
   var dbVal = snapshot.val(),
       dbKeys;
