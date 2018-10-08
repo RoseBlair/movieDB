1534b8b6787ad2790540dcfb26d749c0565dc2fd
chuman <42072991+chrishuman0923@users.noreply.github.com>
2018-10-08 17:23:31 -0400

    Adds remove button, sorts collection alphabetically and incorporates async js library

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -21,14 +21,27 @@ var $movieSearch = $("#movieSearch"),
       lang: "&language=en-US"
     },
     db = firebase.database(),
-    movieIDs;
+    movies = [];
 
 //Adds movie data to database
 function addMovieToDb(movieID, movieTitle) {
+  //clear array before update
+  movies = [];
+
+  //push entry to db
   db.ref().push({
     id: parseInt(movieID),
     title: movieTitle.trim().toLowerCase()
   });
+}
+
+//Removes movie entry from database
+function removeFromDB(key) {
+  //clear array before update
+  movies = [];
+
+  //remove entry from db
+  db.ref().child(key).remove();
 }
 
 function searchMovie(title) {
@@ -47,56 +60,121 @@ function searchMovie(title) {
   });
 }
 
-function displayCollection() {
+function ajaxParallelCalls() {
+  //empty array for calls
+  var ajaxCalls = [];
+
+  //loop through the movies in the collection
+  for(var i = 0; i < movies.length; i++) {
+    var url = [tmdb.idUrl, movies[i].id, tmdb.apiKey, tmdb.lang].join(""),
+        call;
+
+    //assign ajax call to array
+    //add an anonymous function enclosure to create a new level of scope to pass the url through
+    call = (function (url) {
+      return function(callback) {
+        $.ajax({
+          url: url,
+          method: "GET"
+        }).then(function(resp) {
+          callback(null, resp);
+        }).catch(function(err) {
+          callback(err, null);
+        });
+      };
+    }(url));
+
+    ajaxCalls.push(call);
+  }
+
+  //run all ajax calls in parallel of each other
+  async.parallel(ajaxCalls, function(err, resp) {
+    //if error was thown, log it
+    if (err !== null) {
+      console.log(`Async Error: ${err}`);
+    } else {
+      displayCollection(resp);
+    }
+  });
+}
+
+function displayCollection(resp) {  
   //clear DOM
   $movieCollection.empty();
 
-  //loops through each movie in db
-  for (var i = 0; i < movieIDs.length; i++) {
-    var url = [tmdb.idUrl, movieIDs[i], tmdb.apiKey, tmdb.lang].join("");
+  resp.sort(function sortAlphaByTitle(movieA, movieB) {
+    movieA = movieA.title.replace(/^the /gi, "");
+    movieB = movieB.title.replace(/^the /gi, "");
 
-    $.ajax({
-      url: url,
-      method: "GET"
-    }).then(function (resp) {        
-      //if movie poster exists...
-      if (resp.poster_path) {
-        var src = [tmdb.imageUrl, resp.poster_path].join(""),
-            newDiv = $("<div>"),
-            detailsDiv = $("<div>"),
-            movie = $("<img>"),
-            genre = $("<p>"),
-            genreList = [],
-            plot = $("<p>");
-        
-        //add attributes to movie poster
-        movie.attr({
-            "src": src,
-            "alt": resp.title,
-        });
+    if (movieA < movieB) {
+      return -1;
+    } else if (movieA > movieB) {
+      return 1;
+    } else {
+      return 0;
+    }
+  });
 
-        //Loop though genres
-        for (var x = 0; x < resp.genres.length; x++) {
-          genreList.push(resp.genres[x].name);
-        }
+  //display results
+  for (var i = 0; i < resp.length; i++) {
+    var currMovie = resp[i];
 
-        //join genres together into list
-        genre.text(genreList.sort().join(", "));
+    //if movie poster exists...
+    if (currMovie.poster_path) {
+      var src = [tmdb.imageUrl, currMovie.poster_path].join(""),
+          newDiv = $("<div>"),
+          detailsDiv = $("<div>"),
+          movie = $("<img>"),
+          genre = $("<p>"),
+          genreList = [],
+          plot = $("<p>"),
+          btn = $("<btn>"),
+          rowDiv;
+      
+      //add attributes to movie poster
+      movie.attr({
+          "src": src,
+          "alt": currMovie.title
+      }).addClass("movieImg");
 
-        //get movie plot
-        plot.text(resp.overview);
-        
-        //append into div
-        detailsDiv.addClass("movieDetails").append(genre, plot);
-        newDiv.append(movie, detailsDiv);
-
-        //append div to DOM
-        $movieCollection.append(newDiv);
+      //Loop though genres
+      for (var x = 0; x < currMovie.genres.length; x++) {
+        genreList.push(currMovie.genres[x].name);
       }
-    }).catch(function (error) {
-      //catch error message
-      console.log(`Error: ${error}`);
-    });
+
+      //join genres together into list
+      genre.text(genreList.sort().join(", "));
+
+      //get movie plot
+      plot.text(currMovie.overview).addClass("plot");
+      
+      //loop through local array to find firebase key
+      for (var y = 0; y < movies.length; y++) {
+        if (movies[y].id === currMovie.id) {
+          btn.attr("data-key", movies[y].key);
+          //exit loop
+          break;
+        }
+      }
+      
+      //Add classes and text to button
+      btn.addClass("btn btn-danger rmLib").html("&#10008; Remove from Library");
+
+      //append into div
+      detailsDiv.addClass("movieDetails").append(genre, plot);
+      newDiv.append(movie, detailsDiv, btn).addClass("col-lg-4 movieDiv");
+
+      //creates a new row div if the element is the 1st, 4th, etc.
+      if (i % 3 === 0) {
+        rowDiv = $("<div>").addClass("row");
+      }
+      
+      //add movie to row
+      rowDiv.append(newDiv);
+
+      //append row to DOM
+      $movieCollection.append(rowDiv);
+    }
   }
 }
 
@@ -116,22 +194,33 @@ function displayMovieSearch(resp) {
   //loop through results and appends movie poster to DOM
   for (var i = 0; i < resultLimit; i++) {
     var currResp = resp[i],
-        src = [tmdb.imageUrl, currResp.poster_path].join("");
+        src = [tmdb.imageUrl, currResp.poster_path].join(""),
+        inDatabase = false;
     
     //if movie poster exists...
     if (currResp.poster_path) {
       var newDiv = $("<div>"),
           movie = $("<img>"),
-          btn = $("<btn>");
+          btn = $("<btn>"),
+          rowDiv;
       
       //add attributes to movie poster
       movie.attr({
           "src": src,
           "alt": currResp.title,
-      });
+      }).addClass("movieImg");
+
+      //loops through local array of movies to determine if movie exists
+      for (var x= 0; x < movies.length; x++) {
+        if (movies[x].id === currResp.id) {
+          inDatabase = true;
+          //exit for loop
+          break;
+        }
+      }
 
       //change button if the movie already in database
-      if (movieIDs.indexOf(currResp.id) === -1) {
+      if (inDatabase === false) {
         //movie not in collection
         btn.attr({
             "data-id": currResp.id,
@@ -139,14 +228,22 @@ function displayMovieSearch(resp) {
           }).text("Add to Library").addClass("btn btn-primary addLib");
       } else {
         //movie in collection
-        btn.html("&#9747; Already in Library").addClass("btn btn-secondary disabled");
+        btn.html("&#9747; Already in Library").addClass("btn btn-secondary addLib disabled");
       }
 
       //append movie poster and btn to div
-      newDiv.append(movie, btn);
+      newDiv.append(movie, btn).addClass("col-lg-4 movieDiv");
 
-      //append created div to DOM
-      $movieSearch.append(newDiv);
+      //creates a new row div if the element is the 1st, 4th, etc.
+      if (i % 3 === 0) {
+        rowDiv = $("<div>").addClass("row searchRow");
+      }
+      
+      //add movie to row
+      rowDiv.append(newDiv);
+
+      //append row to DOM
+      $movieSearch.append(rowDiv);
     }
   }
 }
@@ -157,31 +254,54 @@ $(document).on("click", ".addLib", function() {
       id = btn.attr("data-id"),
       title = btn.attr("data-title");
 
-    addMovieToDb(id, title);
+    //if the button hasn't been disabled...
+    if (!btn.hasClass("disabled")) {
+      addMovieToDb(id, title);
 
-    //disable button and change text
-    btn.html("&#10004; Added to Library").addClass("disabled btn-secondary").removeClass("btn-primary");
+      //disable button and change text
+      btn.html("&#10004; Added to Library").addClass("disabled btn-secondary").removeClass("btn-primary");
+    }
+});
+
+//event listener for "Remove from Library" btn
+$(document).on("click", ".rmLib", function() {
+  var key = $(this).attr("data-key");
+
+  //remove entry from db
+  removeFromDB(key);
 });
 
 //when movies are added or removed from the database or when the page first loads, create/update the local array
 db.ref().on("value", function(snapshot) {
   var dbVal = snapshot.val(),
       dbKeys;
-    
-  //create empty array for ID's
-  movieIDs = [];
 
   if (dbVal !== null) {
     //get keys
     dbKeys = Object.keys(dbVal);
 
-    //append ID's to array
+    //loop through data
     for (var i = 0; i < dbKeys.length; i++) {
-      movieIDs.push(dbVal[dbKeys[i]].id);
+      var newMovie = {
+            key: dbKeys[i],
+            id: dbVal[dbKeys[i]].id
+          };
+
+      //push object into local array
+      movies.push(newMovie);
     }
 
-    //display existing collection
-    displayCollection();
+    //create ajax calls for existing collection
+    ajaxParallelCalls();
+  } else {
+    //there are no movies to display
+    var header = $("<h3>"),
+        msg = $("<p>");
+
+    header.text("No Movies in Collection").addClass("noMovieMsg");
+    msg.text("Begin adding movies by searching for titles below.").addClass("noMovieMsg");
+
+    $movieCollection.append(header, msg);
   }
 }, function(error) {
   //catch error message
